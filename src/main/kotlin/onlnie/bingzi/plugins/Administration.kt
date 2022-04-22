ad73fe6791bf9096b2a9cf63110aaf7086b4adca
lhby233 <lhby233@outlook.com>
2022-04-22 23:37:16 +0800

    Create-2022年4月22日
    [+] 更改关闭程序URL路径
    [+] 注释JSON WEB AUTH相关具体代码

--- src/main/kotlin/onlnie/bingzi/plugins/Administration.kt
+++ src/main/kotlin/onlnie/bingzi/plugins/Administration.kt
@@ -5,9 +5,9 @@ import io.ktor.server.engine.*
 
 fun Application.configureAdministration() {
     install(ShutDownUrl.ApplicationCallPlugin) {
-        // The URL that will be intercepted (you can also use the application.conf's ktor.deployment.shutdown.url key)
-        shutDownUrl = "/ktor/application/shutdown"
-        // A function that will be executed to get the exit code of the process
+        // 将被拦截的 URL（也可以使用 application.conf 的 ktor.deployment.shutdown.url 键）
+        shutDownUrl = "/shutdown"
+        // 将执行以获取进程的退出代码的函数
         exitCodeSupplier = { 0 } // ApplicationCall.() -> Int
     }
 
