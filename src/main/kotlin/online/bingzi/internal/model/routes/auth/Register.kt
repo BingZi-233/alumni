7d4b9095b00c9129d95e4b65bc16641d309a326f
lhby233 <lhby233@outlook.com>
2022-04-24 23:52:46 +0800

    Create-2022年4月24日
    [+] 更新注释

--- src/main/kotlin/online/bingzi/internal/model/routes/auth/Register.kt
+++ src/main/kotlin/online/bingzi/internal/model/routes/auth/Register.kt
@@ -17,20 +17,32 @@ fun Route.register(path: String) {
     post(path) {
         // 将发送过来的JSON进行序列化
         val userRegister = call.receive(UserRegister::class)
+        // 构建返回对象并对值进行初始化
         val userRegisterResult = UserRegisterResult().apply {
+            // 正则校验账户名是否合法
             this.user = userRegister.user.matches("^[a-zA-Z\\d_-]{2,15}$".toRegex())
+            // 正则校验昵称是否合法
             this.username = userRegister.username.matches("^[a-z\\d\u4e00-\u9fa5]+[^_]\$".toRegex())
+            // 正则校验密码是否合法
             this.password = userRegister.password.matches("^(?!\\d+\$)(?![a-zA-Z]+\$)[\\dA-Za-z]{6,15}\$".toRegex())
+            // 正则校验班级是否合法
             this.clazz = userRegister.clazz.matches("^[a-zA-Z\\d_-]{2,15}\$".toRegex())
+            // 根据上面判断返回值，此刻并不是最终返回值
             this.result = user && username && password && clazz
+            // 构建提示语句
             this.info = if (result) "校验已结束，全部正确！该数据已插入到数据库中。" else "校验失败，有部分参数不正确！请检查您不正确的参数并进行修正。"
         }
+        // 判断检验是否通过
         if (userRegisterResult.result) {
+            // 在数据库中查询该用户是否存在
             AuthSession.registerMapper.queryUserByUser(userRegister.user)?.let {
+                // 用户存在，更改返回值
                 userRegisterResult.result = false
+                // 构建提示语句
                 userRegisterResult.info = "校验失败，该用户名已存在！"
-            } ?: AuthSession.registerMapper.insertUser(userRegister)
+            } ?: AuthSession.registerMapper.insertUser(userRegister) // 用户不存在，将该用户数据插入到数据库中
         }
+        // 返回实体并自动转化为JSON类型返回
         call.respond(userRegisterResult)
     }
 }
--- src/main/kotlin/online/bingzi/internal/model/routes/auth/UnRegister.kt
+++ src/main/kotlin/online/bingzi/internal/model/routes/auth/UnRegister.kt
@@ -15,13 +15,20 @@ import online.bingzi.internal.entity.request.auth.UserUnRegisterResult
  */
 fun Route.unregister(path: String) {
     post(path) {
+        // 将发送过来的JSON进行序列化
         val userUnRegister = call.receive(UserUnRegister::class)
+        // 预构建返回对象，已指定用户名
         val userUnRegisterResult = UserUnRegisterResult(userUnRegister.user)
+        // 在数据库中查询该用户（用户和密码均匹配），并设置返回对象的返回值
         userUnRegisterResult.result = AuthSession.unRegisterMapper.queryUserByUser(userUnRegister)?.let {
+            // 已查到符合的行，对其进行删除
             AuthSession.unRegisterMapper.deleteUser(userUnRegister)
+            // 构建提示语句
             userUnRegisterResult.info = "账户${userUnRegister.user}已被删除"
+            // 返回值设置为真
             true
-        } ?: false
+        } ?: false // 为查询到符合对象，返回值设置为假
+        // 返回实体并自动转化为JSON类型返回
         call.respond(userUnRegisterResult)
     }
 }
